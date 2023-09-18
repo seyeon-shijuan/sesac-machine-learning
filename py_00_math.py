40f3c64f587907f33580df4d0487b94dc5962d97
seyeon <seyeon47@naver.com>
2023-09-18 17:27:24 +0900

    230918

---
+++ py_00_math.py
@@ -0,0 +1,97 @@
+def e109():
+    score1 = 10
+    score2 = 20
+    score3 = 30
+    n_student = 3
+    mean = (score1 + score2 + score3) / n_student
+    print(mean)
+
+    score1 += 10
+    score2 += 10
+    score3 += 10
+    print(score1, score2, score3)
+    mean = (score1 + score2 + score3) / n_student
+    print(mean)
+
+
+def e110():
+    # Mean Subtraction
+    # 평균 값을 구하고, 그 평균을 각 값에서 뺀 다음에 평균을 구하면 0이됨
+    score1 = 10
+    score2 = 20
+    score3 = 30
+    n_student = 3
+
+    score_mean = (score1 + score2 + score3) / n_student
+
+    score1 -= score_mean
+    score2 -= score_mean
+    score3 -= score_mean
+
+    score_mean = (score1 + score2 + score3) / n_student
+    print(score_mean)
+
+
+def e111():
+    score1 = 10
+    score2 = 20
+    score3 = 30
+    n_student = 3
+
+    mean = (score1 + score2 + score3) / n_student
+    square_of_mean = mean**2
+
+    # 분산 = 편차 제곱의 평균
+    mean_of_square = (score1**2 + score2**2 + score3**2) / n_student
+    print("square of mean: ", square_of_mean)
+    print("mean of square: ", mean_of_square)
+
+
+def e112():
+    score1 = 10
+    score2 = 20
+    score3 = 30
+    n_student = 3
+
+    score_mean = (score1 + score2 + score3) / n_student
+    square_of_mean = score_mean**2
+    # 제곱 평균
+    mean_of_square = (score1**2 + score2**2 + score3**2) / n_student
+    # 분산 = 제곱 평균 - 평균 제곱
+    score_variance = mean_of_square - square_of_mean
+    score_std = score_variance ** 0.5
+
+    # ** 0.5 루트인듯?
+    print("mean: ", score_mean)
+    print("variance: ", score_variance)
+    print("standard deviation: ", score_std)
+
+def e113():
+    score1 = 10
+    score2 = 20
+    score3 = 30
+    n_student = 3
+
+    # 평균
+    score_mean = (score1 + score2 + score3) / n_student
+    square_of_mean = score_mean**2 # 평균 제곱
+    mean_of_square = (score1**2 + score2**2 + score3**2) / n_student # 제곱의 평균
+    # 분산 = 제곱의 평균 - 평균의 제곱, 표준편차는 분산에 루트
+    score_variance = mean_of_square - square_of_mean
+    score_std = score_variance**0.5
+    print("mean: ", score_mean)
+    print("standard deviation: ", score_std)
+
+    # score1 =
+
+
+if __name__ == '__main__':
+    # e109()
+    # e110()
+    # e111()
+    # e112()
+    e113()
+
+
+
+
--- py_01_matplotlib.py
+++ py_01_matplotlib.py
@@ -65,16 +65,177 @@ def txt_alignment():
     ax.grid()
     # labelsize 라벨 폰트
     ax.tick_params(axis='both', labelsize=15)
-    ax.text(x=0, y=0, s='hello', fontsize=30)
+    # ax.text(x=0, y=0, s='hello', fontsize=30)
+    # ax.text(x=0.5, y=0, s='hello2', fontsize=30)
+    # ax.text(x=0.5, y=-0.5, s='hello3', fontsize=30)
 
-    ax.text(x=0.5, y=0, s='hello2', fontsize=30)
-    ax.text(x=0.5, y=-0.5, s='hello3', fontsize=30)
+    ax.text(x=0, y=0, va='center', ha='left', s='hello', fontsize=30)
+    # ax.text(x=0, y=0, va='center', ha='center', s='hello2', fontsize=30)
+    # ax.text(x=0, y=0, va='center', ha='right', s='hello3', fontsize=30)
+
+
+def align_test():
+    figsize = (7, 7)
+    fig, ax = plt.subplots(figsize=figsize)
+
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
+
+    ax.grid()
+    # 'top', 'bottom', 'center', 'baseline', 'center_baseline'
+    # 'center', 'right', 'left'
+    # ax.text(x=0, y=0, va='bottom', ha='right', s='Hello', fontsize=30)
+    ax.text(x=0, y=0, va='top', ha='left', s='Hello', fontsize=30)
+
+
+def tick_mt():
+    # figsize = (7, 7)
+    figsize = (14, 7)
+    fig, ax = plt.subplots(figsize=figsize)
+
+    # ax.set_xlim([0, 10])
+    # ax.set_ylim([0, 10])
+    #
+    # ax.set_xlim([0, 10])
+    # ax.set_xticks([0, 1, 5, 10])
+
+    # set xticks (Major and minor ticks)
+    major_xticks = [i for i in range(0, 101, 20)]
+    minor_xticks = [i for i in range(0, 101, 5)]
+
+    ax.set_xticks(major_xticks)
+    ax.set_xticks(minor_xticks, minor=True)
+
+    ax.tick_params(axis='x', labelsize=20, length=10, width=3, rotation=30)
+    ax.tick_params(axis='x', which='minor', length=5, width=2)
+
+
+def tick_test():
+    figsize = (14, 7)
+    fig, ax = plt.subplots(figsize=figsize)
+    major_xticks = [i for i in range(0, 101, 20)]
+    minor_xticks = [i for i in range(0, 101, 5)]
+
+    major_yticks = [i for i in range(0, 11, 2)]
+    minor_yticks = [i for i in range(0, 11)]
+
+    ax.set_xticks(major_xticks)
+    ax.set_xticks(minor_xticks, minor=True)
+
+    ax.set_yticks(major_yticks)
+    ax.set_yticks(minor_yticks, minor=True)
+
+    ax.tick_params(axis='x', labelsize=20, length=10, width=3, rotation=30)
+    ax.tick_params(axis='x', which='minor', length=5, width=2)
+
+    ax.tick_params(axis='y', labelsize=20, length=10, width=3)
+    ax.tick_params(axis='y', which='minor', length=5, width=2)
+
+    ax.tick_params()
+
+
+def named_colors():
+    color_list = ['b', 'g', 'r', 'c', 'm', 'y']
+
+    fig, ax = plt.subplots(figsize=(5, 10))
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, len(color_list)])
+
+    for c_idx, c in enumerate(color_list):
+        # 3번째는 텍스트임
+        ax.text(0, c_idx, "color="+c, fontsize=20, ha='center', color=c)
+
+
+def named_colors2():
+    # 252, 186, 3
+    color_list = [(1., 0., 0.),
+                  (0., 1., 0.),
+                  (0., 0., 1.)]
+
+    fig, ax = plt.subplots(figsize=(5, 10))
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, len(color_list)])
+
+    for c_idx, c in enumerate(color_list):
+        ax.text(0, c_idx, f"color={c}", fontsize=20, ha='center', color=c)
+
+
+def my_scatter():
+    np.random.seed(0)
+
+    n_data = 100
+    # 정규분포로 랜덤값 뽑기
+    x_data = np.random.normal(0, 1, (n_data,))
+    y_data = np.random.normal(0, 1, (n_data,))
+
+    fig, ax = plt.subplots(figsize=(7, 7))
+    # s는 점의 크기(marker size)
+    # ax.scatter(x_data, y_data, s=300, color='r')
+    ##########################
+    # ax.plot(x_data, y_data, 'o', color='red', markersize=10)
+
+    # uniform linspace
+    # x_min, x_max = -5, 5
+    # n_data = 300
+    #
+    # x_data = np.random.uniform(x_min, x_max, n_data)
+    # y_data = x_data + 0.5*np.random.normal(0, 1, n_data)
+    #
+    # pred_x = np.linspace(x_min, x_max, 2)
+    # pred_y = pred_x
+    #
+    # fix, ax = plt.subplots(figsize=(10, 10))
+    # ax.scatter(x_data, y_data)
+    #
+    # ax.plot(pred_x, pred_y, color='r', linewidth=3)
+
+    # size array and color array
+    # n_data = 10
+    # # 양 옆이 정해져있고 일정하게 n_data만큼 추출
+    # x_data = np.linspace(0, 10, n_data)
+    # y_data = np.linspace(0, 10, n_data)
+    #
+    # s_arr = np.linspace(10, 500, n_data)
+    #
+    # fig, ax = plt.subplots(figsize=(10, 10))
+    # # s는 size라서 점점 커짐
+    # ax.scatter(x_data, y_data, s=s_arr)
+
+    # color array
+    # n_data = 10
+    # # linspace 균일한 간격으로 추출
+    # x_data = np.linspace(0, 10, n_data)
+    # y_data = np.linspace(0, 10, n_data)
+    #
+    # c_arr = [(c/n_data, c/n_data, c/n_data) for c in range(n_data)]
+    #
+    # fig, ax = plt.subplots(figsize=(10, 10))
+    # # s= 점 사이즈, c= 점 컬러
+    # ax.scatter(x_data, y_data, s=500, c=c_arr)
+
+    # size array and color array
+    n_data = 500
+    x_data = np.random.normal(0, 1, size=(n_data, ))
+    y_data = np.random.normal(0, 1, size=(n_data, ))
+    s_arr = np.random.uniform(100, 500, n_data)
+    c_arr = [np.random.uniform(0, 1, 3) for _ in range(n_data)]
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    # ax.scatter(x_data, y_data, s=s_arr, c=c_arr)
+
+    ax.scatter(x_data, y_data, s=s_arr, c=c_arr, alpha=0.3)
 
 
 if __name__ == '__main__':
     # base_mat()
     # my_twinx()
-    txt_alignment()
+    # txt_alignment()
+    # align_test()
+    # tick_mt()
+    # tick_test()
+    # named_colors()
+    # named_colors2()
+    my_scatter()
     plt.show()
 
 
